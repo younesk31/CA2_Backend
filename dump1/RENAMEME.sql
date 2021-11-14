d87b18ca77690301a5bc22b9b29d646f9814eb67
Slider <cph-yk31@cphbusiness.dk>
2021-11-14 14:31:50 +0100

    remove secure

--- dump1/RENAMEME.sql
+++
@@ -1,8 +0,0 @@
-create table RENAMEME
-(
-    ID        bigint auto_increment
-        primary key,
-    DUMMYSTR1 varchar(255) null,
-    DUMMYSTR2 varchar(255) null
-);
-
--- dump1/roles.sql
+++
@@ -1,8 +0,0 @@
-create table roles
-(
-    role_name varchar(20) not null
-        primary key
-);
-
-INSERT INTO NameOfDBCA2.roles (role_name) VALUES ('admin');
-INSERT INTO NameOfDBCA2.roles (role_name) VALUES ('user');
